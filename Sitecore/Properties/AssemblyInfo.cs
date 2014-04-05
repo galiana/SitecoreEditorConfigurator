7cb024b85b1f438f7aa40a12bb3141c9745227a5
galiana <vicent@galysoft.es>
2014-04-05 21:00:40 +0100

    First version. It´s possible to manage real font sizes from the content tree.

---
+++ Sitecore/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ab6719b-76c2-4843-9971-c063e5262002")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SitecoreEditorConfigurator/EditorConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Controls.RichTextEditor;
+
+namespace Galiana.SitecoreEditorConfigurator
+{
+    public class EditorConfiguration : Sitecore.Shell.Controls.RichTextEditor.EditorConfiguration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Sitecore.Shell.Controls.RichTextEditor.EditorConfiguration" /> class.
+        /// </summary>
+        /// <param name="profile">
+        /// The profile.
+        /// </param>
+        public EditorConfiguration(Item profile) : base(profile) { }
+        
+
+        /// <summary>
+        /// Builds the font sizes.
+        /// </summary>
+        protected override void SetupFontSizes()
+        {
+            int num;
+            
+            Item Sizesitem = this.Profile.Children["Real Font Sizes"];
+            Item RealSizesitem = this.Profile.Children["Real Font Sizes"];
+            if (Sizesitem != null)
+            {
+                this.Editor.FontSizes.Clear();
+                foreach (Item child in Sizesitem.Children)
+                {
+                    string str = child["Value"];
+                    this.Editor.FontSizes.Add(str);
+                }
+            }
+            if (RealSizesitem != null)
+            {
+                this.Editor.RealFontSizes.Clear();
+                foreach (Item child in RealSizesitem.Children)
+                {
+                    string str = child["Value"];
+                    this.Editor.RealFontSizes.Add(str);
+                }
+            }
+            if (Sizesitem != null && RealSizesitem != null)
+                Log.SingleWarn("Relative and real Font sizes have been configured on profile " + this.Profile.DisplayName + ". Only real sizes will be shown.", this);
+        }
+    }
+}
